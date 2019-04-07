ce9f48edf40f3acc7ed976c0e4e6d8cd3ed05aff
Isiah VanBrackle <squarepheonix@gmail.com>
2019-04-07 01:30:38 -0400

    dropped database, added card model/controller, altered seedsneed to create and migrate tables

--- db/migrate/20190407043628_create_decks.rb
+++ db/migrate/20190407043628_create_decks.rb
@@ -1,9 +1,7 @@
 class CreateDecks < ActiveRecord::Migration[5.2]
   def change
     create_table :decks do |t|
-      t.string :suite
-      t.string :card_type
-      t.string :image 
+
       t.timestamps
     end
   end
