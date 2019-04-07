2691f63372e1eddf8f47b6ca9c5315fa13a2e195
Isiah VanBrackle <squarepheonix@gmail.com>
2019-04-07 03:24:54 -0400

    new database created and migrated

--- db/migrate/20190407060644_create_cards.rb
+++ db/migrate/20190407060644_create_cards.rb
@@ -1,7 +1,10 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
-
+      t.string :suite
+      t.string :card_type
+      t.string :image
+      t.references :deck, foreign_key: true
       t.timestamps
     end
   end
